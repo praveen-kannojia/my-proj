1d3ebd84ccf3fecbeefc542545d8da241f65d342
Kumar <praveen-kumar3@mercer.com>
2016-09-16 20:24:15 +0530

    routing-first change

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule }   from '@angular/forms';
 import {HeroDetailComponent} from './hero-detail.component'
 import { AppComponent }  from './app.component';
 
+
 @NgModule({
   imports: [ 
     BrowserModule,
---
+++ app/heroes.component.ts
@@ -0,0 +1,11 @@
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-heroes',
+})
+export class HeroesComponent implements OnInit {
+
+  ngOnInit(): void{
+    
+  }
+}
